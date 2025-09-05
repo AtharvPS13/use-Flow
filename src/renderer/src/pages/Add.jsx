ebb944d545751ef90c976527eb00c08edb5adb5e
Arunit <arunitchakraborty2006@gmail.com>
2025-09-05 19:33:28 +0530

    feat(frontend): added ui modificatons

--- src/renderer/src/pages/Add.jsx
+++ src/renderer/src/pages/Add.jsx
@@ -1,3 +1,16 @@
+import { useNavigate } from 'react-router-dom'
+
 export default function Add() {
-  return <div>Hello this is the add page</div>
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('/')
+  }
+  return (
+    <div>
+      <p>Hello this is the add page</p>
+      <div className="btn" onClick={handleClick}>
+        <p>Go to home</p>
+      </div>
+    </div>
+  )
 }
